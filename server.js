043e7b0a04806f5d688635c8d284b94fece37575
Alex Goode <alexgoodestudio@gmail.com>
2025-01-19 18:33:39 -0500

    updated server.js.

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use((req, res, next) => {
   res.status(404).send({ error: "Route not found" });
 });
 
-app.listen(process.env.PORT || 5001, () => {
-  console.log(`Server is running on port ${process.env.PORT || 5001}`);
+
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
