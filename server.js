393152954e9169f70a1e68f4bfdb822da3f4bd18
Alex Goode <alexgoodestudio@gmail.com>
2025-01-19 19:01:43 -0500

    updated server.js to handle client side routing

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path"); // Add path to serve the build folder
 const contactRouter = require("./api/contact/contact.router");  
 const app = express();
 
 app.use(cors());  
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "build")));
+
 app.use("/contacts", contactRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.use((req, res, next) => {
   res.status(404).send({ error: "Route not found" });
 });
-
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => {
