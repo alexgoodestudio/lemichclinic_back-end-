d651a609df8f3105fc845ab89694137719dbba15
Alex Goode <alexgoodestudio@gmail.com>
2025-01-19 20:31:00 -0500

    new server file

--- server.js
+++ server.js
@@ -8,15 +8,16 @@ app.use(cors());
 app.use(express.json());
 
 // Serve static files from the 'front-end/build' directory
-app.use(express.static(path.join(__dirname, "../front-end/build")));  // Adjusted path
+app.use(express.static(path.join(__dirname, "../front-end/build")));
 
 app.use("/contacts", contactRouter);
 
 // Serve React app for all other routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../front-end/build", "index.html")); // Adjusted path
+  res.sendFile(path.join(__dirname, "../front-end/build/index.html"));
 });
 
+// Handle 404 for undefined routes
 app.use((req, res, next) => {
   res.status(404).send({ error: "Route not found" });
 });
