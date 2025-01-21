6799f7f4d1ded83a9950137bfaeecd7cc077706c
Alex Goode <alexgoodestudio@gmail.com>
2025-01-20 19:37:17 -0500

    updated build path on sergver

--- server.js
+++ server.js
@@ -7,14 +7,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../front-end/build")));
+// Serve static files from the correct path
+app.use(express.static(path.join(__dirname, "lemich/front-end/build")));
 
 app.use("/contacts", contactRouter);
 
+// Serve the frontend's index.html for any unrecognized route
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../front-end/build/index.html"));
+  res.sendFile(path.join(__dirname, "lemich/front-end/build/index.html"));
 });
 
+// Handle 404 errors for undefined routes
 app.use((req, res, next) => {
   res.status(404).send({ error: "Route not found" });
 });
